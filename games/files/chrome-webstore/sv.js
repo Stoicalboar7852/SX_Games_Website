73b943807b551c7f45ab253c3951459315f7af68
Stoicalboar7852 <108503162+Stoicalboar7852@users.noreply.github.com>
2023-10-23 23:19:38 +1000

    Delete games/files/chrome-webstore/sv.js

--- games/files/chrome-webstore/sv.js
+++
@@ -1,8 +0,0 @@
-importScripts("js/func.js");
-
-onStartup(() => {});
-chrome.action.onClicked.addListener(openGame);
-chrome.runtime.onInstalled.addListener(e => {
-  "install" === e.reason && openGame()
-});
-alarm();
